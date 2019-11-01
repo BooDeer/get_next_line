ff7ca0fa8cd5549bb0a121d713cd49db6830a0f2
BooDeer <hsissi3a@gmail.com>
2019-11-01 18:17:10 +0100

    when it reads a small number of bytes it works fine until the line before the EOF

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 20:26:22 by hboudhir          #+#    #+#             */
-/*   Updated: 2019/11/01 17:21:12 by hboudhir         ###   ########.fr       */
+/*   Updated: 2019/11/01 18:16:25 by hboudhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,22 @@ int		get_next_line(int fd, char **line)
 	if (extra)
 		*line = ft_strjoin(*line, extra);
 	buffer = (char *)malloc(BUFFER_SIZE + 1);
+	free(extra);
 	while (k && (chr = read(fd, buffer, BUFFER_SIZE)))
 	{
+		// printf("*------------------------------------------*\n\t\t\t%d\n", chr);
 		buffer[chr] = '\0';
         if ((ptr = ft_strchr(buffer, '\n')))
         {
             extra = ft_strdup(ptr + 1);
 			*ptr = '\0'; 
             k = 0;
-        } 
+        }
         *line = ft_strjoin(*line ,buffer);
-
+		if (k == 0)
+			return (1);
 	}
-	return (1);
+	return (0);
 }
 
 
@@ -51,14 +54,9 @@ int main(void)
 	i = 0;
 	fd = open("text.txt", O_RDONLY);
 
-	get_next_line(fd, &line);
-	printf("* %s\n", line);
-	get_next_line(fd, &line);
-	printf("* %s\n", line);
-	get_next_line(fd, &line);
-	printf("* %s\n", line);
-	get_next_line(fd, &line);
-	printf("* %s\n", line);	
+	while(get_next_line(fd, &line))
+		printf("* %s\n", line);
+
 	close(fd);
 	return (0);
 }	
