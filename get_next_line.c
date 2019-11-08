7a7a7a84e9da2c068cde23466ce707e73b552e4c
Houssam-eddine Boudhir <hboudhir@e2r6p8.1337.ma>
2019-11-08 15:06:30 +0100

    IT WORKS

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 20:26:22 by hboudhir          #+#    #+#             */
-/*   Updated: 2019/11/06 21:22:50 by hboudhir         ###   ########.fr       */
+/*   Updated: 2019/11/08 15:04:47 by hboudhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,14 @@ char			*check_reminder(char *reminder, char **line)
 	return (p_n);
 }
 
+int		check_return(int br, char *reminder, char **line, char *buffer)
+{
+	if (!br && ft_strlen(reminder) == 0)
+		return (0);
+	if (br || ft_strlen(reminder) || ft_strlen(*line))
+		return (1);
+	return (br);
+}
 int				get_next_line(int fd, char **line)
 {
 	static char		*reminder;
@@ -55,11 +63,11 @@ int				get_next_line(int fd, char **line)
 	char			*p_n;
 	char			*tmp;
 
-	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!fd || !(buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
 	p_n = check_reminder(reminder, line);
-	while (!p_n && (br = read(fd, buffer, BUFFER_SIZE)))
-	{
-		buffer[br] = '\0';
+	while (!p_n && (br = read(fd, buffer, BUFFER_SIZE)))	
+	{	buffer[br] = '\0';
 		if ((p_n = ft_strchr(buffer, '\n')))
 		{
 			*p_n = '\0';
@@ -69,10 +77,10 @@ int				get_next_line(int fd, char **line)
 		tmp = *line;
 		*line = ft_strjoin(*line, buffer);
 		free(tmp);
+		
 	}
-	if (BUFFER_SIZE > br)
-		return (0);
-	return ((br || ft_strlen(reminder) || ft_strlen(*line)) ? 1 : 0);
+	free(buffer);
+	return (check_return(br, reminder, line, buffer));
 }
 
 int main(void)
