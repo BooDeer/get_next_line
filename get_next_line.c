0bee88370d416bdd29cb328e6f984cadb19705dc
Houssam-eddine Boudhir <hboudhir@e2r6p8.1337.ma>
2019-11-09 23:44:55 +0100

    get next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 20:26:22 by hboudhir          #+#    #+#             */
-/*   Updated: 2019/11/08 18:00:39 by hboudhir         ###   ########.fr       */
+/*   Updated: 2019/11/09 23:02:01 by hboudhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,31 +25,35 @@ size_t			ft_strlen(const char *s)
 	return (i);
 }
 
-char			*check_reminder(char *reminder, char **line)
+int			check_reminder(char *reminder, char **line)
 {
 	char *p_n;
 
 	p_n = NULL;
+	*line = ft_strdup("");
 	if (reminder)
+	{
 		if ((p_n = ft_strchr(reminder, '\n')))
 		{
 			*p_n = '\0';
 			*line = ft_strdup(reminder);
 			ft_strcpy(reminder, ++p_n);
+			return (1);
 		}
 		else
 		{
 			*line = ft_strdup(reminder);
 			ft_memset(reminder, 0, ft_strlen(reminder));
 		}
-	else
-		*line = ft_strdup("\0");
-	return (p_n);
+	}
+	//else
+		//*line = ft_strdup("");
+	return (0);
 }
 
-int				check_return(int br, char *reminder, char **line, char *buffer)
+int				check_return(int br, char *reminder, char **line)
 {
-	if (!br && ft_strlen(reminder) == 0)
+	if (!br && !ft_strlen(reminder))
 		return (0);
 	if (br || ft_strlen(reminder) || ft_strlen(*line))
 		return (1);
@@ -64,22 +68,43 @@ int				get_next_line(int fd, char **line)
 	char			*ptr;
 	char			*tmp;
 
-	if (!fd || !(rd = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+	if (BUFFER_SIZE < 0 || fd < 0 || read(fd, NULL, 0) < 0 || !(rd = malloc((BUFFER_SIZE + 1))))
 		return (-1);
-	ptr = check_reminder(bf, line);
-	while (!ptr && (br = read(fd, rd, BUFFER_SIZE)))
+	if (check_reminder(bf, line))
+		return (1);
+	while ((br = read(fd, rd, BUFFER_SIZE)))
 	{
 		rd[br] = '\0';
 		if ((ptr = ft_strchr(rd, '\n')))
 		{
-			*ptr = '\0';
-			ptr++;
+			*ptr++ = '\0';
 			bf = ft_strdup(ptr);
+			*line = ft_strjoin(*line, rd);
+			return (1);
 		}
 		tmp = *line;
 		*line = ft_strjoin(*line, rd);
 		free(tmp);
 	}
 	free(rd);
-	return (check_return(br, bf, line, rd));
+	return (check_return(br, bf, line));
 }
+// int main()
+// {
+// 	int fd;
+// 	char *line;
+// 	int i = 0, rd;
+// 	fd = open("normal.txt", O_RDONLY);
+
+// 	while ((
+// 		rd = get_next_line(42, &line)
+// 	))
+// 	{
+// 		printf("%d-%d* %s\n", i++,rd , line);
+// 		//free(line);
+// 	}
+// 	/*get_next_line(fd, &line);
+// 	printf("%d-%d* %s\n", i++, rd, line);
+// 	free(line);*/
+
+// }
